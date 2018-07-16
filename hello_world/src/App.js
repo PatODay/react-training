1d6c721a08f549af4c2f546ef53ffeeb57c9e406
PatODay <poday22@gmail.com>
2018-07-15 21:00:40 -0400

    Cleaning up some commented out code
    Cleaned up some commented out code from hello_world

--- hello_world/src/App.js
+++ hello_world/src/App.js
@@ -1,33 +1,9 @@
-// import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-//
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h1 className="App-title">Welcome to React</h1>
-//         </header>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     );
-//   }
-// }
-//
-// export default App;
-
 // bring in React and Component from React
 import React, {Component} from 'react';
 
 // define our Hello component
 class Hello extends Component {
-  // what should the component render?
   render () {
-    // make sure to return some UI
     return (
       <div>
       <h1>Hello {this.props.name}!</h1>
